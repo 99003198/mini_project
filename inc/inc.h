e4b657e9c371a478f71bc7d87885b8ba0c70cbce
99003198 <sappa.swarnadurga@ltts.com>
2020-12-05 14:12:03 +0530

    commit

---
+++ inc/inc.h
@@ -0,0 +1,9 @@
+#ifndef __CHANGE_H__
+define __CHANGE_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void change();
+
+#endif
---
+++ src/change.c
@@ -0,0 +1,10 @@
+#include "change.h"
+ 
+void change()
+{
+int c,a=5,b=8;
+c=a;
+a=b;
+b=c;
+printf("%d %d",a,b);
+}
